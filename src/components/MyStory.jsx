7a24b0c1136e4b528db1293289d2046b65c93cc7
NjeruKariuki <freakoutbond2@gmail.com>
2024-02-05 12:14:08 +0300

    added my skills section:

--- src/components/MyStory.jsx
+++ src/components/MyStory.jsx
@@ -7,8 +7,8 @@ function MyStory(props) {
 
                 <div className="w-full md:pr-8">
                     <h1 className="mb-4 text-2xl font-bold text-white md:text-4xl">My Story</h1>
-                    <p className="mb-4 font-mono text-lg md:text-xl">Growing up in Nairobi, Kenya, I was always fascinated by computers and technology. I spent countless hours tinkering with hardware and software, eager to learn how they worked.</p>
-                    <p className="mb-4 font-mono text-lg md:text-xl">Despite facing many challenges along the way, including limited access to resources and a lack of formal education in computer science, my passion for technology never wavered.</p>
+                    <p className="mb-4 font-mono text-lg md:text-xl">Growing up in the cold highlands of Kenya, I was always fascinated by computers and technology. I spent countless hours tinkering with hardware and software, eager to learn how they worked.</p>
+                    <p className="mb-4 font-mono text-lg md:text-xl">Despite facing many challenges along the way, my passion for technology never wavered. I now use the power of mathematics and code to build a bright future for my generation</p>
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 
 function Navbar(props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleLinkClick = (sectionId) => {
+        // Smooth scroll to the specified section
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+
+        // Close the navbar on mobile after clicking a link
+        setIsOpen(false);
+    };
+
     return (
-        <nav class="bg-gray-900 p-4 shadow-xl">
-            <div class="max-w-7xl mx-auto flex justify-between items-center">
-                <div class="text-white text-2xl font-bold">CHIEF</div>
-                <div class="flex space-x-6">
-                    <a href="#intro" class="text-white hover:text-green-500 transition duration-300">Intro</a>
-                    <a href="#mystory" class="text-white hover:text-green-500 transition duration-300">My Story</a>
-                    <a href="#skills" class="text-white hover:text-green-500 transition duration-300">Skills</a>
-                    <a href="#projects" class="text-white hover:text-green-500 transition duration-300">Projects</a>
+        <nav className="p-4 bg-gray-900 shadow-xl">
+            <div className="flex items-center justify-between mx-auto max-w-7xl">
+                <div className="flex items-center text-2xl font-bold text-white">DNK</div>
+                <div className="md:hidden">
+                    {isOpen ? (
+                        // Close icon when navbar is open
+                        <button onClick={handleToggle} className="text-white focus:outline-none">
+                            <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    ) : (
+                        // Hamburger menu icon when navbar is closed
+                        <button onClick={handleToggle} className="text-white focus:outline-none">
+                            <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    )}
+                </div>
+                <div className={`md:flex md:items-center space-x-6 ${isOpen ? 'block' : 'hidden'} md:w-auto w-full md:space-x-0 md:mt-0 mt-4 md:justify-end gap-4`}>
+                    {/* Navbar links */}
+                    <a href="#intro" onClick={() => handleLinkClick('intro')} className="block text-white transition duration-300 hover:text-green-500">Intro</a>
+                    <a href="#mystory" onClick={() => handleLinkClick('mystory')} className="block text-white transition duration-300 hover:text-green-500">My Story</a>
+                    <a href="#skills" onClick={() => handleLinkClick('skills')} className="block text-white transition duration-300 hover:text-green-500">Skills</a>
+                    <a href="#projects" onClick={() => handleLinkClick('projects')} className="block text-white transition duration-300 hover:text-green-500">Projects</a>
                 </div>
             </div>
         </nav>
@@ -17,9 +51,4 @@ function Navbar(props) {
 }
 
 export default Navbar;
-
-
-
-
-
 
